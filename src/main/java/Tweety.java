32e1f5c676e17cd3cd8a2212caaf067e8e7188b3
soomin-k <oncesoomin0930@gmail.com>
2025-08-22 00:31:52 +0800

    level 1 completed

--- src/main/java/Tweety.java
+++ src/main/java/Tweety.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Tweety {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Tweety");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in); // define scanner to read user inputs
+
+        System.out.println("     ____________________________________________________________");
+        System.out.println("     Hello! I'm Tweety");
+        System.out.println("     What can I do for you?");
+        System.out.println("     ____________________________________________________________");
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("     ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("     ____________________________________________________________");
+                break;
+            }
+
+            System.out.println("     ____________________________________________________________");
+            System.out.println("     " + userInput);
+            System.out.println("     ____________________________________________________________");
+        }
     }
 }
